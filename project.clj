b59221e7467863e9af964589602f350518afe51e
radist <rdst101@gmail.com>
2018-12-23 11:38:30 +0700

    change java version

--- project.clj
+++ project.clj
@@ -16,5 +16,5 @@
   :ring {:handler youtube-to.core/app}
   :plugins [[lein-ring "0.12.1"] 
             [lein-kibit "0.1.6"]]
-  :min-lein-version "2.8.1"
+  :min-lein-version "2.7.1"
   :profiles {:uberjar {:aot :all}})
